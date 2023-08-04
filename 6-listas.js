7d4a1153be35a1ce57a0d6767113ac05d027bc8a
wesley-siejka <126927077+wesley-siejka@users.noreply.github.com>
2023-08-04 09:02:16 -0300

    Add files via upload

---
+++ 6-listas.js
@@ -0,0 +1,15 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio De Janeiro`;
+
+const listaDeDestinos = new Array(
+    `salvador`,
+    `São Paulo`,
+    `Rio De Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`) //adicionando um item na lista
+console.log("Destinos possiveis:");
+// console.log(salvador, saoPaulo, rioDeJaneiro);
+console.log(listaDeDestinos);
